f7de216e379f7d2d133c66a0e3fd4c80f7a39332
Boris Rakas <radionicarakas@gmail.com>
2020-03-17 12:00:48 +0100

    lead page section done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   
+    function checkHeader() {
+        let headerHeight = $('header').innerHeight();
+        $('main').css('margin-top', headerHeight);
+    }
     
+    checkHeader();
+    $(window).resize(function () {
+        checkHeader();
+    });
     
 });
